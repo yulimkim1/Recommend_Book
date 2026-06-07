6c65963395c02261a171dfd5586ba518138159df
Yulim Kim <yulimkim@email.unc.edu>
2026-06-07 12:47:29 -0400

    repo set up and folder structure/requirements added

---
+++ test_api.py
@@ -0,0 +1,19 @@
+import requests
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
+
+def search_books(title):
+    url = "https://www.googleapis.com/books/v1/volumes"
+    params = {"q": title, "key": API_KEY, "maxResults": 5}
+    response = requests.get(url, params = params)
+    data = response.json()
+
+    for item in data.get("items", []):
+        book_info = item["volumeInfo"]
+        print(f"{book_info.get('title')} by {book_info.get('authors', ['Unknown'])}")
+
+search_books("Harry Potter")
